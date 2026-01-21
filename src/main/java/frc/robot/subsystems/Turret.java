0408b37f98c23ce5cb3b711cd5a241cc2bc358f9
Gloc0de <YOUR_EMAIL>
2026-01-21 14:34:22 -0800

    Added turret

---
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.LimelightHelpers;
+
+public class Turret {
+  double Kp = -0.1;
+  double min_command = 0.05;
+  // program motor
+  private final LimelightHelpers LimelightHelpers = new LimelightHelpers();
+
+  public Command aimShooter() {
+    return Commands.runOnce(() -> {});
+  }
+
+  // private Command Turn
+  private void trunMotors() {
+    while (true) {}
+  }
+
+  // set a while loop outside of command that checks up on lime data( treshhold are we aimed or not
+  // ) - with that data aim motor test
+
+}
