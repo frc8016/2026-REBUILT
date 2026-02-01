5b45c3b7bc04685a7fd7c45ecc791ea358c6c120
Gloc0de <YOUR_EMAIL>
2026-02-01 13:37:36 -0800

    new format

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -6,22 +6,23 @@ import edu.wpi.first.wpilibj2.command.Commands;
 // import frc.robot.LimelightHelpers;
 
 public class Turret {
-  double Kp = -0.1;
-  double min_command = 0.05;
+    double Kp = -0.1;
+    double min_command = 0.05;
 
-  // program motor
-  // private final LimelightHelpers LimelightHelpers = new LimelightHelpers();
+    // program motor
+    // private final LimelightHelpers LimelightHelpers = new LimelightHelpers();
 
-  public Command aimShooter() {
-    return Commands.runOnce(() -> {});
-  }
+    public Command aimShooter() {
+        return Commands.runOnce(() -> {});
+    }
 
-  // private Command Turn
-  private void trunMotors() {
-    while (true) {}
-  }
+    // private Command Turn
+    private void trunMotors() {
+        while (true) {}
+    }
 
-  // set a while loop outside of command that checks up on lime data( treshhold are we aimed or not
-  // ) - with that data aim motor
+    // set a while loop outside of command that checks up on lime data( treshhold are we aimed or
+    // not
+    // ) - with that data aim motor
 
 }
