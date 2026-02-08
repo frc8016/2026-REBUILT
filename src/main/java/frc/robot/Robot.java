c495e1b2f8b7d2a1f1811bf424890138f840f2f7
Jeremiah Welch <jeremiahwwelch@gmail.com>
2026-02-07 18:04:05 -0800

    passing vision measurements to drivetrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,9 +8,11 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.PhotonVision;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
+    private PhotonVision photonVision;
 
     private final RobotContainer m_robotContainer;
 
@@ -20,12 +22,14 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         m_robotContainer = new RobotContainer();
+        photonVision = new PhotonVision(m_robotContainer.drivetrain);
     }
 
     @Override
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run();
+        photonVision.updateVision();
     }
 
     @Override
