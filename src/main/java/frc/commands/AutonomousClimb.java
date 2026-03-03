8892feb926ea7e2294ca68c6c83e90347175fdd8
jeremiah-welch <jeremiahwwelch@gmail.com>
2026-03-03 13:52:38 -0800

    start of auto align for climb

---
+++ src/main/java/frc/commands/AutonomousClimb.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.commands;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.FileVersionException;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import frc.robot.Constants.AutonomousClimbConstants;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
+import org.json.simple.parser.ParseException;
+
+public class AutonomousClimb {
+    private Supplier<Pose2d> swervePoseSupplier;
+    private Optional<Alliance> alliance = DriverStation.getAlliance();
+
+    Supplier<Command> pathfindToClimb =
+            () -> {
+                String pathName = constructPathName(getClosestPoseName());
+                Optional<PathPlannerPath> maybePath = loadPath(pathName);
+
+                if (maybePath.isPresent()) {
+                    return AutoBuilder.pathfindThenFollowPath(
+                            maybePath.get(), AutonomousClimbConstants.constraints);
+                } else {
+                    return Commands.none();
+                }
+            };
+
+    public AutonomousClimb(Supplier<Pose2d> poseSupplierIn) {
+        swervePoseSupplier = poseSupplierIn;
+    }
+
+    public Command createPathfindToClimb() {
+        return new DeferredCommand(
+                pathfindToClimb, Set.of(swervePoseSupplier.get())); // not needed deferred command
+    }
+
+    private String getClosestPoseName() {
+        Pose2d pose =
+                swervePoseSupplier
+                        .getState()
+                        .Pose
+                        .nearest(new ArrayList<>(getLineupPoseMap().keySet()));
+        return getLineupPoseMap().get(pose);
+    }
+
+    private Map<Pose2d, String> getLineupPoseMap() {
+        if (alliance.isPresent() && alliance.get() == Alliance.Red) {
+            return AutonomousClimbConstants.RED_CLIMB_POSITIONS;
+        } else {
+            return AutonomousClimbConstants.BLUE_CLIMB_POSITIONS;
+        }
+    }
+
+    private Optional<PathPlannerPath> loadPath(String pathName) {
+        try {
+            return Optional.of(PathPlannerPath.fromPathFile(pathName));
+        } catch (FileVersionException | IOException | ParseException e) {
+            System.out.println("error in loading path");
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    private String constructPathName(String pointName) {
+        return "climb_" + pointName;
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,13 +10,13 @@ import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.FollowPathCommand;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.commands.AutonomousClimb;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.BallisticsManager;
 import frc.robot.subsystems.BottomFlywheel;
@@ -115,12 +115,8 @@ public class RobotContainer {
         joystick.a().onTrue(topFlywheel.sysId());
         joystick.b()
                 .whileTrue(
-                        drivetrain.applyRequest(
-                                () ->
-                                        point.withModuleDirection(
-                                                new Rotation2d(
-                                                        -joystick.getLeftY(),
-                                                        -joystick.getLeftX()))));
+                        new AutonomousClimb(() -> drivetrain.getState().Pose)
+                                .createPathfindToClimb());
 
         joystick.rightTrigger()
                 .whileTrue(
