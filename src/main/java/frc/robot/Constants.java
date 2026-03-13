03bcd45079da9957eaf478483cb198b42350a8dc
jeremiah-welch <jeremiahwwelch@gmail.com>
2026-03-13 12:12:51 -0700

    continue tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -215,10 +215,10 @@ public final class Constants {
         public static final Time OPEN_LOOP_RAMP_RATE = Seconds.of(0.25);
         public static final Current STATOR_CURRENT_LIMIT = Amps.of(40);
         public static final double IS_READY_DELAY = 0.05;
-        public static final Angle BOTTOM_SOFT_LIMIT = Degrees.of(70);
-        public static final Angle TOP_SOFT_LIMIT = Degrees.of(40);
+        public static final Angle BOTTOM_SOFT_LIMIT = Degrees.of(41);
+        public static final Angle TOP_SOFT_LIMIT = Degrees.of(65);
         public static final Distance HOOD_LENGTH = Inches.of(17.8);
-        public static final Angle START_ANGLE = Degrees.of(70);
+        public static final Angle START_ANGLE = Degrees.of(65);
         public static final Mass HOOD_WEIGHT = Kilograms.of(1);
     }
 
