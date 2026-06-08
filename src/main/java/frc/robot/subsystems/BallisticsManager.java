b31d9e507d9656b3ca28a27d5ae82210f12c95ed
Jeremiah Welch <58541179+jeremiah-welch@users.noreply.github.com>
2026-06-07 19:01:57 -0700

    Revert "added shoot on the move"

--- src/main/java/frc/robot/subsystems/BallisticsManager.java
+++ src/main/java/frc/robot/subsystems/BallisticsManager.java
@@ -3,18 +3,17 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.MetersPerSecond;
-import static frc.robot.Constants.BallisticsManagerConstants.*;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.BallisticsManagerConstants;
 import frc.robot.Constants.TurretConstants;
 import java.util.function.Supplier;
 
@@ -22,28 +21,20 @@ public class BallisticsManager extends SubsystemBase {
 
     private final Supplier<Pose3d> targetPoseSupplier;
     private final Supplier<Pose2d> robotPoseSupplier;
-    private final Supplier<Angle> turretAngleSupplier; // CCW negative
-    private final Supplier<ChassisSpeeds> chassisSpeedsSupplier;
 
     private LinearVelocity flywheelVelocity = MetersPerSecond.of(0);
     private Angle hoodAngle = Degree.of(0);
     private Angle targetHorizontalAngle = Degree.of(0);
     private double targetDistanceMeters = 0;
     private double tempTargetHorizontalAngle = 0;
-    private double compensatedTargetDistanceMeters = 0;
 
     // private static TunableNumber flywheelMps = new TunableNumber("flywheelMps", 10);
     // private static TunableNumber hoodDeg = new TunableNumber("hoodDeg", 40);
 
     public BallisticsManager(
-            Supplier<Pose3d> targetPoseSupplier,
-            Supplier<Pose2d> robotPoseSupplier,
-            Supplier<Angle> turretAngleSupplier,
-            Supplier<ChassisSpeeds> chassisSpeedsSupplier) {
+            Supplier<Pose3d> targetPoseSupplier, Supplier<Pose2d> robotPoseSupplier) {
         this.targetPoseSupplier = targetPoseSupplier;
         this.robotPoseSupplier = robotPoseSupplier;
-        this.turretAngleSupplier = turretAngleSupplier;
-        this.chassisSpeedsSupplier = chassisSpeedsSupplier;
     }
 
     @Override
@@ -62,24 +53,15 @@ public class BallisticsManager extends SubsystemBase {
 
         if (targetDistanceMeters < 1E-6) return;
 
-        double tof = TIME_OF_FLIGHT.get(targetDistanceMeters);
+        targetHorizontalAngle = Degrees.of(tempTargetHorizontalAngle);
 
-        Translation2d turretFieldVelocity =
-                computeTurretFieldVelocity(
-                        chassisSpeedsSupplier.get(),
-                        robotPoseSupplier.get().getRotation(),
-                        turretAngleSupplier.get());
+        // Lookup ballistics tables using horizontal distance
+        double flywheelMps =
+                BallisticsManagerConstants.FLYWHEEL_SPEED_MAP.get(targetDistanceMeters);
+        double hoodDeg = BallisticsManagerConstants.HOOD_ANGLE_MAP.get(targetDistanceMeters);
 
-        Translation2d compensatedTargetTranslation =
-                targetTranslation.plus(turretFieldVelocity.times(tof));
-        compensatedTargetDistanceMeters = compensatedTargetTranslation.getNorm();
-
-        double compensatedFlywheelMps = FLYWHEEL_SPEED_MAP.get(compensatedTargetDistanceMeters);
-        double compensatedHoodDeg = HOOD_ANGLE_MAP.get(compensatedTargetDistanceMeters);
-
-        flywheelVelocity = MetersPerSecond.of(compensatedFlywheelMps);
-        hoodAngle = Degree.of(compensatedHoodDeg);
-        targetHorizontalAngle = compensatedTargetTranslation.getAngle().getMeasure();
+        flywheelVelocity = MetersPerSecond.of(flywheelMps);
+        hoodAngle = Degree.of(hoodDeg);
 
         // targetHorizontalAngle = targetTranslation.getAngle().getMeasure();
 
@@ -125,28 +107,5 @@ public class BallisticsManager extends SubsystemBase {
             // Apply to the robot's global pose
             return targetTranslation;
         };
-    }
-
-    private Translation2d computeTurretFieldVelocity(
-            ChassisSpeeds chassisSpeeds, Rotation2d robotHeading, Angle turretAngle) {
-        // Rotate pivot offset by turret angle (CCW negative)
-        Rotation2d turretRotation = Rotation2d.fromDegrees(-turretAngle.in(Degrees));
-        Translation2d rotatedOffset = TurretConstants.TURRET_OFFSET.rotateBy(turretRotation);
-
-        // Angular velocity contribution at pivot
-        double omega = chassisSpeeds.omegaRadiansPerSecond;
-        double vxRot = -omega * rotatedOffset.getY(); // -ω * y
-        double vyRot = omega * rotatedOffset.getX(); // ω * x
-
-        // Robot-relative velocity at turret
-        double vxRobot = chassisSpeeds.vxMetersPerSecond + vxRot;
-        double vyRobot = chassisSpeeds.vyMetersPerSecond + vyRot;
-
-        // Rotate to field frame
-        double cos = robotHeading.getCos();
-        double sin = robotHeading.getSin();
-        double vxField = vxRobot * cos - vyRobot * sin;
-        double vyField = vxRobot * sin + vyRobot * cos;
-        return new Translation2d(vxField, vyField);
     }
 }
